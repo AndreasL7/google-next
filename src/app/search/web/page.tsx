bf65603fd193eae366727f63795d9a53af8f300f
Andreas Lukita <andreas030503@gmail.com>
2024-12-06 09:10:46 +0800

    handle possible errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import Link from "next/link";
 
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
+interface SearchParamsProps {
+  searchParams: {
+    searchTerm: string;
+  };
+}
+
+interface SearchResult {
+  title: string;
+  link: string;
+  snippet: string;
+  // Add additional properties based on your API response
+}
+
+const WebSearchPage = async ({ searchParams }: SearchParamsProps) => {
+  const searchParamsAwaited = await searchParams;
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParamsAwaited.searchTerm}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  const data = await response.json();
+  const results: SearchResult[] = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found {searchParamsAwaited.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {results &&
+        results.map((result: SearchResult, idx) => (
+          <h1 key={idx}>{result.title}</h1>
+        ))}
+    </div>
+  );
 };
 
 export default WebSearchPage;
